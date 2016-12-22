310be597035026430193c166cb04e46585fe7dbd
Maicon Diniz Filippsen <filippsen@blockie.org>
2016-12-22 02:52:32 +0100

    Add base conformance tests

---
+++ README.md
@@ -0,0 +1,9 @@
+# Spacechecker
+
+Spacechecker is a _Space_ module that performs analysis of other modules, looking for a set of properties and testing for conformance with _Space_ modules guidelines.
+
+## Usage
+
+```
+space -m spacechecker /run/ -- /path/to/module
+```
---
+++ Spacefile.bash
@@ -0,0 +1,86 @@
+#
+# Copyright 2016 Blockie AB
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#
+# spacechecker - Space module analyzer
+#
+
+_CHECK_DEP_INSTALL_NODE()
+{
+    space -f "$_dir_name/Spacefile.yaml" /_dep_install/ -h > /dev/null 2>&1
+    if [ "$?" -ne 0 ]; then
+        PRINT "expected _dep_install node in Spacefile.yaml" "warning"
+    else
+        PRINT "OK" "success"
+    fi
+}
+
+_CHECK_LICENSE_FILE_EXISTS()
+{
+    if [ -f "$_dir_name/LICENSE" ]; then
+        PRINT "OK" "success"
+    else
+        PRINT "expected LICENSE file" "warning"
+    fi
+}
+
+_CHECK_TESTS_EXIST()
+{
+    if [ -d "$_dir_name/test" ]; then
+        if [ -f "$_dir_name/test/test.yaml" ] \
+            && [ -f "$_dir_name/test/test.sh" ]; then
+            PRINT "OK" "success"
+        else
+            PRINT "expected test.yaml,sh in the test directory " "warning"
+        fi
+    else
+        PRINT "expected test directory" "warning"
+    fi
+}
+
+_CHECK_MODULE()
+{
+    SPACE_CMDDEP="PRINT _CHECK_DEP_INSTALL_NODE _CHECK_LICENSE_FILE_EXISTS _CHECK_TESTS_EXIST"
+
+    if [ "$#" -eq 0 ]; then
+        PRINT "missing module directory path to analyze" "error"
+        return 1
+    fi
+
+    local _dir_name="$1"
+    # Append PWD if path is relative
+    if [[ ! "${_dir_name:0:1}" = "/" ]]; then
+        _dir_name="${PWD}/${_dir_name}"
+    fi
+
+    # Check for Spacefile.sh|.bash,yaml
+    if [ -f "$_dir_name/Spacefile.yaml" ]; then
+        if [ -f "$_dir_name/Spacefile.sh" ] \
+           || [ -f "$_dir_name/Spacefile.bash" ]; then
+            PRINT "OK" "success"
+            _CHECK_DEP_INSTALL_NODE
+            _CHECK_LICENSE_FILE_EXISTS
+            _CHECK_TESTS_EXIST
+        else
+            PRINT "expected Spacefile.sh|bash in directory: $_dir_name" "error"
+            exit 1
+        fi
+    else
+        PRINT "expected Spacefile.yaml in directory: $_dir_name" "error"
+        exit 1
+    fi
+}
+
