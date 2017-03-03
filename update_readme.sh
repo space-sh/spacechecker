795fe61a085f65a3bb5267255a7b5687ee161e19
Maicon Diniz Filippsen <filippsen@blockie.org>
2017-03-03 11:44:50 +0100

    Apply new update_readme

--- update_readme.sh
+++ update_readme.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
 
-GENERATE_VARIABLES=0 GENERATE_TOC=0 space -f ../space/tools/spacedoc/Spacefile.yaml /module/ -- Spacefile.sh && \
+space -m autodoc /export/ -- Spacefile.sh && \
     mv Spacefile.sh_README README.md && printf "README.md has been overwritten\n"
-
